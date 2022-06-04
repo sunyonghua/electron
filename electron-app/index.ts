9077c0e1d127711c0f50d2ecec73b54f5fc664a2
yong hua <yonghua.s@icloud.com>
2022-06-04 08:41:00 +0800

    esm

--- electron-app/index.ts
+++ electron-app/index.ts
@@ -1,5 +1,5 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+import { app, BrowserWindow } from 'electron'
+import path from 'path'
 
 function createWindow() {
   const main = new BrowserWindow({
